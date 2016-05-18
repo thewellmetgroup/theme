c6443de6b3b8aeee58ce42f85aaf0c7dac70a48c
Cyril Tsiboulski <ct@cloudred.com>
2016-05-18 18:29:20 -0400

    added header and basics

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,23 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+    <div class="row">
+        <div class="col-md-4">
+            <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/dist/images/wellmet-logo.png" alt="<?php bloginfo('name'); ?>" width="250" height="250">
+            </a>
+        </div>
+        <div class="col-md-8">
+            <nav class="nav-primary">
+              <?php
+              if (has_nav_menu('primary_navigation')) :
+                wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+              endif;
+              ?>
+            </nav>
+            <div class="mission">
+                <?php echo get_bloginfo('description'); ?>
+            </div>
+        </div>
+    </div>
   </div>
 </header>
