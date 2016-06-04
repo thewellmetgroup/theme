c6b2f3b3ca0a6efdeca181b07455b5ea7c61a24b
Cyril Tsiboulski <ct@cloudred.com>
2016-06-03 21:38:04 -0400

    added cases for displaying grantee blocks

--- templates/content-grantee.php
+++ templates/content-grantee.php
@@ -1,21 +1,86 @@
 <?php 	$grantee_images= get_field('images');
-		if ($grantee_images) {
-			shuffle( $grantee_images );
+		$grantee_mission=get_field('featured_mission');
+		$grantee_description=get_field('description');
+		
+		function trunc($phrase, $max_words) {
+   			$phrase_array = explode(' ',$phrase);
+   			if(count($phrase_array) > $max_words && $max_words > 0)
+      			$phrase = implode(' ',array_slice($phrase_array, 0, $max_words)).'...';
+   			return $phrase;
 		}
-	    $i=0;
-	    //print_r($grantee_images);
-	    foreach( $grantee_images as $image ): 
-			if ($i==0) {
-				$grantee_image_url_0 = $image['sizes']['large'];
-				$grantee_image_alt_0 = $image['alt'];
-			}
-			if ($i==1) {
-				$grantee_image_url_1 = $image['sizes']['large'];
-				$grantee_image_alt_1 = $image['alt'];
-				break;
-			}
-			$i++;
-	  	endforeach;
+		
+		//if the grantee mission if empty
+		if (empty($grantee_mission)) {
+			//set description excerpt as mission
+			$grantee_mission = trunc($grantee_description,25);
+		}
+		
+		//if there are images for this grantee
+		if (!empty($grantee_images)) {
+			//if more than 1
+			if (count($grantee_images)>1) {
+				shuffle( $grantee_images );
+				$i=0;
+	    		//print_r($grantee_images);
+	    		foreach( $grantee_images as $image ): 
+					if ($i==0) {
+						$full_img_url = $image['sizes']['large'];
+						$full_img_alt = $image['alt'];
+					}
+					if ($i==1) {
+						$halfy_img_url = $image['sizes']['large'];
+						$halfy_img_alt = $image['alt'];
+						break;
+					}
+					$i++;
+	  			endforeach;
+	  		} else {
+	  			//if just one
+	  			$single_img_url = $grantee_images[0]['sizes']['large'];
+				$single_img_alt = $grantee_images[0]['alt'];
+	  		}
+	  	}
+	  	
+	  	$rand=rand(1,4);
+	  	$colors = array('#79b118','#00c3df','#ca195c','#f4c021');
+	  	//#79b118; //green
+	  	//#00c3df; //blue
+		//#ca195c; //purple
+		//#f4c021; //orange
+	  	//shuffle 
+	  	shuffle($colors);
+	  	
+	  	$picked_color=$colors[$rand-1];
+	  	//print "RAND: ".$rand;
+	  	//print "<br>PICKED: ".$picked_color;
+	  	
+	  	//create div arrays so we can randomize block positions
+	  	function fuller() {
+	  		global $full_img_url;
+	  		global $picked_color;
+	  		return '<div class="full bgstyle" style="background-image:url(\''.$full_img_url.'\')">
+	  					 <div class="title">
+        					<span class="header" style="color:'.$picked_color.';">Recent Grantee</span><br>
+        					<span class="name">'.get_the_title().'</span>
+        				 </div>
+        			   </div>';
+	  	}
+        
+        function halfy($pos,$bg_type) {
+        	global $grantee_mission;
+        	global $picked_color;
+        	global $halfy_img_url;
+        	global $halfy_img_alt;
+        	if ($bg_type=='image') {
+        		$css_value = 'background-image:url(\''.$halfy_img_url.'\')';
+        		$halfy_content="";
+        	} else {
+        		$css_value = 'background-color:'.$picked_color.''; //must be the color
+        		$halfy_content=$grantee_mission;
+        	}
+        	return '<div class="halfy '.$pos.' bgstyle" style="'.$css_value.'"><div class="halfy-content">'.$halfy_content.'</div></div>';
+        }
+        
 ?>
 
 <!--<div class="grantee-detail">
@@ -28,23 +93,56 @@
 
 <div class="grantee-detail">
     <div class="row gutter-15">
+        <?php
+        //do this switcharoo if there are images and there's more than 1
+        if ( count($grantee_images)>1 ) { ?>
         <div class="col-sm-6">
-        	<div class="full bgstyle" style="background-image:url('<?php echo $grantee_image_url_0; ?>')">
-        		<?php //if (!is_home()): ?>
-        			<div class="title">
-        				<span class="header">Recent Grantee</span><br>
-        				<span class="name"><?php the_title(); ?></span>
-        			</div>
-        		<?php //endif; ?>
-			</div>
-        	
+        	<?php 
+        	switch ($rand) {
+    			case "1":
+        			echo fuller();
+        			break;
+    			case "2":
+        			echo halfy('top','image');
+        			echo halfy('bottom','color');
+        			break;
+    			case "3":
+        			echo halfy('top','color');
+        			echo halfy('bottom','image');
+        			break;
+        		case "4":
+        			echo fuller();
+        			break;
+			} ?> 	
         </div>
         <div class="col-sm-6 right">
-			<div class="halphy top bgstyle" style="background-image:url('<?php echo $grantee_image_url_1; ?>')">
-			</div>
-			<div class="halphy bottom" style="background-color:red;">
-			</div>
+			<?php 
+        	switch ($rand) {
+    			case "1":
+        			echo halfy('top','image');
+        			echo halfy('bottom','color');
+        			break;
+    			case "2":
+        			echo fuller();
+        			break;
+    			case "3":
+        			echo fuller(); 
+        			break;
+        		case "4":
+        			echo halfy('top','color');
+        			echo halfy('bottom','image');
+        			break;
+			} ?>
         </div>
+        <?php } else { ?>
+        	<div class="col-xs-12">
+        		<?php if (count($grantee_images)==1) { ?>
+        			<div class="single-image"><img src="<?php echo $single_img_url; ?>" alt="<?php echo $single_img_alt; ?>"></div>
+        		<?php } ?>
+        		<div><?php echo $grantee_description; ?></div>
+        	</div>
+        
+        <?php }  ?>
     </div>
 </div>
 
